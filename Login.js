bbf5d070e9985b068e051df7d749d9ae4ea4348b
Surya <usurya1995@gmail.com>
2019-04-25 17:57:34 +0530

    Update Login.js

--- Login.js
+++ Login.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
+import FooterComponent from './FooterComponent';
 import './Login.css';
 
-class App extends React.Component {
-   constructor() {
-      super();
+class Login extends Component {
+   constructor(props) {
+      super(props);
       this.state = {
          user: "",
-         pass: "",
-         errors: {}
+         pass: ""
       }
       this.updateUser = this.updateUser.bind(this);
       this.updatePass = this.updatePass.bind(this);
@@ -23,29 +23,37 @@ class App extends React.Component {
    }
 
    updateDetails(e) {
-      e.preventDefault();
-      alert("Username:" + this.state.user + "," + "Password" + this.state.pass);
+      //e.preventDefault();
+      alert("Username:" + this.state.user + "," + "Password:" + this.state.pass);
+      this.props.history.push(`/home/${this.state.user}`);
    }
 
    render() {
       return (
          <div>
+            <div className="container-fluid">
             <div className="row">
-               <div className="col-md-4"></div>
                <div className="col-md-4">
-                  <h1>Login</h1>
+                  <img src="image1.png" alt="CISCO" className="img-size"></img>
+               </div>
+               <div className="col-md-4">
+                  <h1>LOGIN</h1>
                   <form onSubmit={this.updateDetails} className="login-form">
                      <label>Username:</label>
-                     <input type="text" className="form-control" value={this.state.user} onChange={this.updateUser} /><br />
+                     <input type="text" className="form-control" value={this.state.user} onChange={this.updateUser} required/><br />
                      <label>Password:</label>
-                     <input type="password" className="form-control" value={this.state.pass} onChange={this.updatePass} /><br />
+                     <input type="password" className="form-control" value={this.state.pass} onChange={this.updatePass} required/><br />
                      <input type="submit" value="Submit" />
                   </form>
                </div>
                <div className="col-md-4"></div>
             </div>
+            <footer>
+                  <FooterComponent></FooterComponent>
+            </footer>
+            </div>
          </div>
       );
    }
 }
-export default App;
+export default Login;
