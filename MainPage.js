d9e8d055236f85deb4cc658cd13dd64a4a3f0c83
Surya <usurya1995@gmail.com>
2019-04-26 11:47:44 +0530

    Update MainPage.js

--- MainPage.js
+++ MainPage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './MainPage.css';
 import FooterComponent from './FooterComponent';
 var data = require('./details.json');
 
@@ -11,21 +12,42 @@ class MainPage extends Component {
     render() {
         return (
             <div>
-                <div>
-                    <p>Main Page</p>
-                    <p>Welcome to:{this.props.match.params.username}</p>
-                    <div className="mainPage">
-                        {/* <p>{data.name}</p> */}
-                        <ul>
-                            {
-                                data[0].name.map((val,i) => {
-                                    return <li key={i}>{val}</li>
-                                })
-                            }
-                        </ul>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <p>Main Page</p>
+                            <p>Welcome to:{this.props.match.params.username}</p>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-3">
+                            {/* <p>{data.name}</p> */}
+                                <ul>
+                                    {
+                                        data[0].name.map((val, i) => {
+                                            return <li key={i}>{val}</li>
+                                        })
+                                    }
+                                </ul>
+                        </div> 
+                        <div className="col-md-9">
+                            <form>
+                                <label>TACAS IP ADDRESS<i className="far fa-asterisk"></i></label>
+                                <br></br>
+                                <input type="text"></input>
+                                <br></br>
+                                <label>KEY</label>
+                                <br></br>
+                                <input type="text"></input>
+                            </form>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-12">
+                            <FooterComponent></FooterComponent>
+                        </div>
                     </div>
                 </div>
-                <FooterComponent></FooterComponent>
             </div>
         );
     }
