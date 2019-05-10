ee2e4f0f8453f1928bed82efde320c53953cef04
Surya <usurya1995@gmail.com>
2019-05-10 09:10:18 +0530

    Create Third.js

---
+++ Components/thirdcomponent/Third.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class Third extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+
+    render() {
+        return (
+            <div>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <h1>Third Component</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+export default Third;
